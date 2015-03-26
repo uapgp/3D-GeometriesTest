f11ccddb3a3f4e42c0420c3c94cd746c5591c0ee
BucketLord <uapgp88@gmail.com>
2015-03-26 23:12:21 +0100

    Revert "Revert "follow displacment""
    This reverts commit 9982a17e72fcf29ef784812bfa6fa91c92b46a92.

--- core/src/geometries/Cubes/Cube2.java
+++ core/src/geometries/Cubes/Cube2.java
@@ -15,7 +15,7 @@ public class Cube2 {
 		// TODO Auto-generated constructor stub
 		
 		ModelBuilder modelBuilder = new ModelBuilder();
-		model = modelBuilder.createBox(5f, 5f, 5f,new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+		model = modelBuilder.createBox(50f, 5f, 5f,new Material(ColorAttribute.createDiffuse(Color.TEAL)),
 				Usage.Position | Usage.Normal);
 		
 		
